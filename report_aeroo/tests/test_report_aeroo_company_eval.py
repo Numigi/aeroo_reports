49f9f78e2d9f7aeaf64a2f72db4a8bea5e5168d1
David Dufresne <dufresne.david.2@gmail.com>
2018-04-26 10:17:32 -0400

    Fix codacy issues

--- report_aeroo/tests/test_report_aeroo_company_eval.py
+++ report_aeroo/tests/test_report_aeroo_company_eval.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
-# © 2016-2018 Savoir-faire Linux
 # © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import base64
-import os
-import stat
-from PIL import Image
-from odoo.exceptions import ValidationError
-from odoo.modules import module
 from odoo.tests import common
 
 

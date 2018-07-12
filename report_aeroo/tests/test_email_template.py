4abac27f32b0b48d90bb2465228cf739f250c308
David Dufresne <dufresne.david.2@gmail.com>
2018-07-12 16:15:35 -0400

    Codacy fix

--- report_aeroo/tests/test_email_template.py
+++ report_aeroo/tests/test_email_template.py
@@ -1,10 +1,6 @@
-# -*- coding: utf-8 -*-
-# © 2016-2018 Savoir-faire Linux
 # © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import base64
-from odoo.modules import module
 from odoo.tests import common
 
 

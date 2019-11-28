1c3ea303735d59fdbf4f96d347eae8448eb3a505
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2019-11-28 10:32:28 -0500

    Add missing access to aeroo manager
    This access is required to delete an aeroo report.

---
+++ report_aeroo/tests/test_report_aeroo_access.py
@@ -0,0 +1,28 @@
+# © 2016-2018 Savoir-faire Linux
+# © 2018 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import base64
+import os
+from freezegun import freeze_time
+
+from odoo.exceptions import ValidationError
+from odoo.modules import module
+from odoo.tests import common
+
+
+class TestAerooReportAccess(common.SavepointCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.report = cls.env.ref('report_aeroo.aeroo_sample_report')
+        cls.user = cls.env.ref('base.user_demo')
+        cls.user.groups_id |= cls.env.ref('report_aeroo.group_aeroo_manager')
+
+    def test_report_create(self):
+        assert self.report.sudo(self.user).copy({})
+
+    def test_report_unlink(self):
+        self.report.sudo(self.user).unlink()
+        assert not self.report.exists()

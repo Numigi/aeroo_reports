c518c6cdc9d2354493132b64efdd0791a1e032e3
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2022-08-04 08:22:59 -0400

    TA#45954 add delta parameter to aeroo utilities (#54)

--- report_aeroo/extra_functions.py
+++ report_aeroo/extra_functions.py
@@ -10,7 +10,7 @@ import itertools
 import logging
 import time
 from babel.core import localedata
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from html2text import html2text
 from io import BytesIO
 from PIL import Image
@@ -45,13 +45,16 @@ class AerooFunctionRegistry(object):
         """
         if func_name in self._functions:
             raise RuntimeError(
-                'A function named {func_name} is already registered in the Aeroo registry.'
-                .format(func_name=func_name))
+                "A function named {func_name} is already registered in the Aeroo registry.".format(
+                    func_name=func_name
+                )
+            )
         self._functions[func_name] = func
 
     def get_functions(self):
         """Get all functions registered inside the registry."""
         return dict(self._functions)
+
 
 aeroo_function_registry = AerooFunctionRegistry()
 
@@ -61,6 +64,7 @@ def aeroo_util(function_name):
 
     :param function_name: the function name available to call the function in Aeroo
     """
+
     def decorator(func):
         aeroo_function_registry.register(function_name, func)
         return func
@@ -68,7 +72,7 @@ def aeroo_util(function_name):
     return decorator
 
 
-@aeroo_util('format_hours')
+@aeroo_util("format_hours")
 def format_hours(report, value):
     hours = str(int(abs(value) // 1))
     minutes = str(int((abs(value) * 60) % 60))
@@ -82,7 +86,7 @@ def _with_padding_zero(value):
     return f"0{value}" if len(value) == 1 else value
 
 
-@aeroo_util('format_date')
+@aeroo_util("format_date")
 def format_date(report, value: date, date_format: str):
     """Format a date field value into the given format.
 
@@ -93,18 +97,22 @@ def format_date(report, value: date, date_format: str):
     :param format: the format to use
     """
     if not value:
-        return ''
-    lang = report._context.get('lang') or 'en_US'
+        return ""
+    lang = report._context.get("lang") or "en_US"
     return babel.dates.format_date(value, date_format, locale=lang)
 
 
-@aeroo_util('today')
-def format_date_today(report, date_format: str = None):
+@aeroo_util("today")
+def format_date_today(report, date_format: str = None, delta: timedelta = None):
     today_in_timezone = fields.Date.context_today(report)
+
+    if delta is not None:
+        today_in_timezone += delta
+
     return format_date(report, value=today_in_timezone, date_format=date_format)
 
 
-@aeroo_util('format_datetime')
+@aeroo_util("format_datetime")
 def format_datetime(report, value: datetime, datetime_format: str):
     """Format a datetime field value into the given format.
 
@@ -115,32 +123,38 @@ def format_datetime(report, value: datetime, datetime_format: str):
     :param format: the format to use
     """
     if not value:
-        return ''
-    lang = report._context.get('lang') or 'en_US'
+        return ""
+    lang = report._context.get("lang") or "en_US"
     datetime_in_timezone = fields.Datetime.context_timestamp(report, value)
-    return babel.dates.format_datetime(datetime_in_timezone, datetime_format, locale=lang)
+    return babel.dates.format_datetime(
+        datetime_in_timezone, datetime_format, locale=lang
+    )
 
 
-@aeroo_util('now')
-def format_datetime_now(report, datetime_format: str = None):
+@aeroo_util("now")
+def format_datetime_now(report, datetime_format: str = None, delta: timedelta = None):
     timestamp = datetime.now()
+
+    if delta is not None:
+        timestamp += delta
+
     return format_datetime(report, value=timestamp, datetime_format=datetime_format)
 
 
-@aeroo_util('format_decimal')
-def format_decimal(report, amount: float, amount_format='#,##0.00'):
+@aeroo_util("format_decimal")
+def format_decimal(report, amount: float, amount_format="#,##0.00"):
     """Format an amount in the language of the user.
 
     :param report: the aeroo report
     :param amount: the amount to format
     :param amount_format: an optional format to use
     """
-    lang = report._context.get('lang') or 'en_US'
+    lang = report._context.get("lang") or "en_US"
     return babel.numbers.format_decimal(amount, format=amount_format, locale=lang)
 
 
-def get_locale_from_odoo_lang_and_country(lang: str, country: 'res.country'):
-    locale = '{}_{}'.format(lang.split('_')[0], country.code)
+def get_locale_from_odoo_lang_and_country(lang: str, country: "res.country"):
+    locale = "{}_{}".format(lang.split("_")[0], country.code)
 
     if not localedata.exists(locale):
         locale = lang
@@ -148,8 +162,10 @@ def get_locale_from_odoo_lang_and_country(lang: str, country: 'res.country'):
     return locale
 
 
-@aeroo_util('format_currency')
-def format_currency(report, amount: float, currency=None, amount_format=None, country=None):
+@aeroo_util("format_currency")
+def format_currency(
+    report, amount: float, currency=None, amount_format=None, country=None
+):
     """Format an amount into the given currency in the language of the user.
 
     :param report: the aeroo report
@@ -159,49 +175,53 @@ def format_currency(report, amount: float, currency=None, amount_format=None, co
     """
     context = report._context
 
-    lang = context.get('lang') or 'en_US'
-    country = country or context.get('country')
+    lang = context.get("lang") or "en_US"
+    country = country or context.get("country")
     locale = get_locale_from_odoo_lang_and_country(lang, country) if country else lang
 
-    currency = currency or context.get('currency')
+    currency = currency or context.get("currency")
     if currency is None:
-        raise ValidationError(_(
-            "The function `format_currency` can not be evaluated without a currency. "
-            "You must either define a currency in the field `Currency Evaluation` of the "
-            "Aeroo report or call the function with a currency explicitely."
-        ))
+        raise ValidationError(
+            _(
+                "The function `format_currency` can not be evaluated without a currency. "
+                "You must either define a currency in the field `Currency Evaluation` of the "
+                "Aeroo report or call the function with a currency explicitely."
+            )
+        )
 
-    return babel.numbers.format_currency(amount, currency.name, format=amount_format, locale=locale)
+    return babel.numbers.format_currency(
+        amount, currency.name, format=amount_format, locale=locale
+    )
 
 
-@aeroo_util('asimage')
+@aeroo_util("asimage")
 def asimage(
     report,
     field_value: str,
     rotate: bool = None,
     size_x: int = None,
     size_y: int = None,
-    uom: str = 'px',
-    hold_ratio: bool = False
+    uom: str = "px",
+    hold_ratio: bool = False,
 ):
     def size_by_uom(val, uom, dpi):
-        if uom == 'px':
-            result = str(val / dpi) + 'in'
-        elif uom == 'cm':
-            result = str(val / 2.54) + 'in'
-        elif uom == 'in':
-            result = str(val) + 'in'
+        if uom == "px":
+            result = str(val / dpi) + "in"
+        elif uom == "cm":
+            result = str(val / 2.54) + "in"
+        elif uom == "in":
+            result = str(val) + "in"
         return result
 
     if not field_value:
-        return BytesIO(), 'image/png'
+        return BytesIO(), "image/png"
 
     field_value = base64.decodebytes(field_value)
     tf = BytesIO(field_value)
     tf.seek(0)
     im = Image.open(tf)
     format = im.format.lower()
-    dpi_x, dpi_y = map(float, im.info.get('dpi', (96, 96)))
+    dpi_x, dpi_y = map(float, im.info.get("dpi", (96, 96)))
 
     if rotate is not None:
         im = im.rotate(int(rotate))
@@ -222,30 +242,34 @@ def asimage(
             elif size_x2 > size_x:
                 size_y = size_y2
 
-    size_x = size_x and size_by_uom(size_x, uom, dpi_x) or str(im.size[0] / dpi_x) + 'in'
-    size_y = size_y and size_by_uom(size_y, uom, dpi_y) or str(im.size[1] / dpi_y) + 'in'
-    return tf, 'image/%s' % format, size_x, size_y
+    size_x = (
+        size_x and size_by_uom(size_x, uom, dpi_x) or str(im.size[0] / dpi_x) + "in"
+    )
+    size_y = (
+        size_y and size_by_uom(size_y, uom, dpi_y) or str(im.size[1] / dpi_y) + "in"
+    )
+    return tf, "image/%s" % format, size_x, size_y
 
 
-@aeroo_util('barcode')
+@aeroo_util("barcode")
 def barcode(
     report,
     code: str,
-    code_type: str = 'ean13',
+    code_type: str = "ean13",
     height: int = 50,
     rotate: bool = None,
     xw: int = 1,
 ):
     if code:
-        if code_type.lower() == 'ean13':
+        if code_type.lower() == "ean13":
             bar = EanBarCode()
             im = bar.getImage(code, height)
-        elif code_type.lower() == 'code128':
+        elif code_type.lower() == "code128":
             im = get_code(code, xw, height)
-        elif code_type.lower() == 'code39':
+        elif code_type.lower() == "code39":
             im = create_c39(height, xw, code)
     else:
-        return BytesIO(), 'image/png'
+        return BytesIO(), "image/png"
 
     stream = _stream_image(im)
 
@@ -253,10 +277,10 @@ def barcode(
         im = im.rotate(int(rotate))
 
     size_x, size_y = _get_image_size(im)
-    return stream, 'image/png', size_x, size_y
+    return stream, "image/png", size_x, size_y
 
 
-@aeroo_util('qrcode')
+@aeroo_util("qrcode")
 def qrcode(report, code, size=None):
     image = make_qr_code(code)
 
@@ -265,12 +289,12 @@ def qrcode(report, code, size=None):
     else:
         size_x, size_y = size, size
 
-    return _stream_image(image), 'image/png', size_x, size_y
+    return _stream_image(image), "image/png", size_x, size_y
 
 
 def _stream_image(image):
     stream = BytesIO()
-    image.save(stream, 'png')
+    image.save(stream, "png")
     return stream
 
 
@@ -281,10 +305,10 @@ def _get_image_size(image):
 
 
 def _to_inches(pixels):
-    return str(pixels / 96.0) + 'in'
+    return str(pixels / 96.0) + "in"
 
 
-@aeroo_util('html2text')
+@aeroo_util("html2text")
 def format_html2text(report, html: str):
     """Convert the given HTML field value into text.
 
@@ -293,14 +317,16 @@ def format_html2text(report, html: str):
     :param html: the html string to format into raw text
     :return: the raw text
     """
-    return html2text(html or '', bodywidth=0)
+    return html2text(html or "", bodywidth=0)
 
 
 @aeroo_util("group_by")
 def group_by(report, records, func, sort=None):
     """Iterate over records grouped by the given comparator function."""
     sorted_records = records.sorted(key=func)
-    groupby_items = ((key, list(group)) for key, group in itertools.groupby(sorted_records, func))
+    groupby_items = (
+        (key, list(group)) for key, group in itertools.groupby(sorted_records, func)
+    )
 
     sorted_groupby_items = (
         groupby_items

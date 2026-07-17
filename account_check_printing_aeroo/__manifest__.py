63c5ad5efa6d3a7c4307e5233917d6d380489901
Abdellatif BENZBIRIA <a.benzbiria@gmail.com>
2026-07-17 09:11:00 +0000

    TA#81785 [18.0] [IMP] account_check_printing_aeroo : move print check group to advanced settings (#91)
    * [18.0][MIG] account_check_printing_aeroo
    
    * [IMP] account_check_printing_aeroo : move print check group to advanced settimgs
    
    * [IMP] account_check_printing_aeroo : move print check group to advanced settimgs
    
    ---------
    
    Co-authored-by: Abdellatif Benzbiria <odoo-base@numigi.com>

--- account_check_printing_aeroo/__manifest__.py
+++ account_check_printing_aeroo/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Aeroo Check Printing",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "author": "Numigi",
     "maintainer": "numigi",
     "website": "http://www.savoirfairelinux.com",
